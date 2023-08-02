93b2b1070156934c00b7f0c6bb5f91ed8b4d4a1c
AlexandreSTMarques <alexandregahan@gmail.com>
2023-08-02 11:36:01 -0300

    atualizar aula11

---
+++ Aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 45
+console.log(`A velocidade do seu carro é ${vel}`)
+if(vel > 60){
+    console.log(`Voce ultrapassou a velocidade permitida. Multado!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
