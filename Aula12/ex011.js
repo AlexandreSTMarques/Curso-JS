9995dce596df1625c35baa84bc27ffd5be74b41b
AlexandreSTMarques <alexandregahan@gmail.com>
2023-08-23 09:54:29 -0300

    criei a aula 012

---
+++ Aula12/ex011.js
@@ -0,0 +1,9 @@
+var idade = 
+console.log(`você tem ${idade} anos`)
+if (idade <16){
+    console.log('Não vota')
+}else if(idade < 18 || idade >63){
+    console.log('Voto opcional')
+} else{
+    console.log('Voto obrigatório')
+}
