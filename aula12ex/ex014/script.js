39b631edcc3c3e5538d6d897013772e74947a72b
AlexandreSTMarques <alexandregahan@gmail.com>
2023-09-25 16:50:21 -0300

    atualizar ex014

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -2,6 +2,22 @@ function carregar(){
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
+    
     var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas.`
+    if(hora >= 0 && hora < 12){
+        //bom dia! 
+        img.src = 'manhã.png'
+        document.body.style.background = 'lightblue'
+    } else if (hora >= 12 && hora < 18){
+        // boa tarde !
+        img.src = 'tarde.png'
+        document.body.style.background = 'deepblue'
+    } else{
+        // boa noite ! 
+        img.src = 'noite.png'
+        document.body.style.background = 'black'
+    } 
+ 
+
 }
