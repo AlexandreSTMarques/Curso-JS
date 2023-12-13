b7fdcd93a64174542c24837188f9fa172915f79d
AlexandreSTMarques <alexandregahan@gmail.com>
2023-12-13 15:42:27 -0300

    atualizar ex016

--- ex016/script.js
+++ ex016/script.js
@@ -4,19 +4,26 @@ function contar(){
     let passo = document.getElementById('txtp')
     let resul = document.getElementById('resul')
     if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0 ){
-        window.alert('[ERRO] faltam dados!')
+        resul.innerHTML = 'Impossível contar'
+       //window.alert('[ERRO] faltam dados!')
     } else {
-        resul.innerHTML = 'Contando:'
+        resul.innerHTML = 'Contando: <br>' 
         let i = Number(inicio.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
+        if(p <= 0){
+            window.alert('[PASSO INVÁLIDO!] considerando passo 1') 
+            p = 1
+        }
      if(i < f){
+        // contagem crescente
          for(let c = i; c <= f;c += p){
         resul.innerHTML += `${c} \u{1F449}`
       }
       resul.innerHTML += `\u{1F44A}`
       
      }else{
+        // contagem regressiva
         for(let c = i; c >= f; c -= p ){
             resul.innerHTML += `${c} \u{1F449}`
         }
